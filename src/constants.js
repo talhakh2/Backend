f6cc8201270b2acce0215ea1c90ab7d05ddf23eb
talhakh <talha.kh58@gmail.com>
2024-06-30 18:42:16 +0500

    MongoDB Connection

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-$null  index.js
+export const DB_NAME = "videotube"
--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-$null 
+import dotenv from 'dotenv'
+import connectDB from '../db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
